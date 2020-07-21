e976be3362bde0a923781e8556ac1a456ae1753e
mo3az <51883008+mo3az96@users.noreply.github.com>
2020-07-21 09:23:35 +0200

    cats

--- js/main.js
+++ js/main.js
@@ -17,29 +17,7 @@ $(document).ready(function () {
         navText: ['<svg xmlns="http://www.w3.org/2000/svg" width="15.839" height="26" viewBox="0 0 15.839 26"><path id="Path_91" data-name="Path 91" d="M1638.6,383.74l12.928,11.588L1638.6,406.916" transform="translate(-1637.187 -382.328)" fill="none"  stroke-linecap="round" stroke-width="2"/></svg>',
             '<svg xmlns="http://www.w3.org/2000/svg" width="15.839" height="26" viewBox="0 0 15.839 26"><path id="Path_91" data-name="Path 91" d="M1651.527,383.74,1638.6,395.328l12.928,11.588" transform="translate(-1637.101 -382.328)" fill="none"  stroke-linecap="round" stroke-width="2"/></svg>'],
     });
-    ///////// **cats Slider** ///////// 
-    if ($(window).width() <= 1199) {
-        $('.cats').addClass("owl-carousel")
-        $('.cats').owlCarousel({
-            margin: 26,
-            rtl: document.dir == 'rtl' ? true : false,
-            loop: true,
-            dots: true,
-            nav: false,
-            responsive: {
-                0: {
-                    items: 1,
-                    maegin: 15,
-                },
-                767: {
-                    items: 2,
-                },
-                991: {
-                    items: 3,
-                },
-            }
-        });
-    }
+  
     ///////// **products Slider** ///////// 
     $('.productsSlider').owlCarousel({
         margin: 10,
